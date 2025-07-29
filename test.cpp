c4c541a5479dd00fac45427abac7e85b803b3111
longle9927 <longl220709@gmail.com>
2025-07-29 20:55:48 +0700

    file temp de test code

--- test.cpp
+++ test.cpp
@@ -10,8 +10,7 @@ const int N=1e6+5;
 const int MOD=1e9+7;
 #define el cout<<'\n'
 using namespace std;
-int n,m;
-char a[N][N];
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -22,34 +21,10 @@ int main()
         freopen(".out","w",stdout);
     }
     //input:
-    cin>>n>>m;
-    F(i,1,n,1) F(j,1,m,1) cin>>a[i][j];
+    
     clock_t begin = clock();
     //code:
-    F(i,1,n,1) F(j,1,m,1) a[i][0]=a[0][j]='Z';
-    F(i,1,n,1) F(j,1,m,1)
-    {
-        bool ok=0;
-        F(c,'A','D',1)
-        {
-            if(a[i][j]!=char(c)&&a[i-1][j]!=char(c)&&a[i][j-1]!=char(c))
-            {
-                a[i][j]=char(c);
-                ok=1;
-                break;
-            }
-        }
-        if(!ok)
-        {
-            cout<<"IMPOSSIBLE";el;
-            return 0;
-        }
-    }
-    F(i,1,n,1)
-    {
-        F(j,1,m,1) cout<<a[i][j]<<' ';
-        el;
-    }
+    
     clock_t end = clock();
     cerr<<endl<<"Time run: "<<(float)(end-begin)/CLOCKS_PER_SEC<<" s"<<endl;
     return 0;
